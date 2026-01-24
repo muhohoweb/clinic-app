ccf3f07e311bab72bd128a5eb851feb335973e38
muhohoweb <muhohoweb@icloud.com>
2026-01-24 12:47:46 +0300

    Remove landing page

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,17 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
+
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
+    return redirect('/login');
 })->name('home');
+
+
+//Route::get('/', function () {
+//    return Inertia::render('Welcome', [
+//        'canRegister' => Features::enabled(Features::registration()),
+//    ]);
+//})->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
